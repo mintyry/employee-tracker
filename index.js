be7bc2368bfc8f4a977993160f8c7258e4abaac9
mintyry <gitmintyry@gmail.com>
2023-11-08 20:23:08 -0800

    made queries.js for functions, adding balues into tables

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const mysql = require('mysql2');
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        database: 'manageco',
-        user: 'root',
-        password: ''
-    },
-    console.log('Thanks for visiting my database.\nIt\'s nice to finally have some company around here.\nLet\'s get to work, shall we?')
-);
+const { viewAllEmployees, addEmployee, updateEmployeeRole, viewAllRoles, addRole, viewAllDepartments, addDepartment, leave } = require('./queries.js')
+// const mysql = require('mysql2');
+// const db = mysql.createConnection(
+//     {
+//         host: 'localhost',
+//         database: 'manageco',
+//         user: 'root',
+//         password: ''
+//     },
+//     console.log('Thanks for visiting my database.\nIt\'s nice to finally have some company around here.\nLet\'s get to work, shall we?')
+// );
 
 const questions = [
     //initial menu question
@@ -35,31 +36,29 @@ function manageCoMenu() {
         .prompt(questions)
         .then(function (data) {
             if (data.menu === 'View all employees') {
-                console.log('employees');//replace with dbquery function
+                viewAllEmployees();
                 manageCoMenu();
             } else if (data.menu === 'Add employee') {
-                console.log('added e');//replace with dbquery function
+                addEmployee();
                 manageCoMenu();
             } else if (data.menu === 'Update employee role') {
-                console.log('updated e');//replace with dbquery function
+                updateEmployeeRole();
                 manageCoMenu();
             } else if (data.menu === 'View all roles') {
-                console.log('roles');//replace with dbquery function
+                viewAllRoles();
                 manageCoMenu();
             } else if (data.menu === 'Add role') {
-                console.log('added r');//replace with dbquery function
+                addRole();
                 manageCoMenu();
             } else if (data.menu === 'View all departments') {
-                console.log('depts');//replace with dbquery function
+                viewAllDepartments();
                 manageCoMenu();
             } else if (data.menu === 'Add department') {
-                console.log('added d');//replace with dbquery function
+                addDepartment();
                 manageCoMenu();
             } else {
-                console.log('Fine, leave me.');
-                process.exit();
+                leave();
             }
-           
         });
 };
 
