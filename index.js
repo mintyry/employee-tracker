aef7278ccb713019c9776b7ed94b180079cd5669
mintyry <gitmintyry@gmail.com>
2023-11-11 20:52:13 -0800

    view all employees shows all required data except manager name

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ function menu() {
 
 
 function viewAllEmployees() {
-    db.query('SELECT * FROM employees', (err, data) => {
+    db.query('SELECT employees.id, employees.first_name, employees.last_name, roles.title, roles.salary, departments.name AS department, employees.manager_id  FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.department_id = departments.id', (err, data) => {
         if (err) {
             console.log('Welp, that ERROR wasn\'t meant to happen.')
         } else {
